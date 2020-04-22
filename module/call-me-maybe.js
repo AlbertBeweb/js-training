efd5c8a9de3d1ad9fd2ad7f19ddbb69ca7335a1d
OGordoo <marcosafonso8@hotmail.com>
2020-04-22 13:05:50 +0100

    Adding if exists then

--- module/call-me-maybe.js
+++ module/call-me-maybe.js
@@ -1,9 +1,20 @@
 export const next = 'object'
 export const exercises = {
-  // each / map
-  // filter / reject / partition
-  // fold / foldr / reduce
-  // every / some
+  // map: { feedback: true }, //add map ex based on box https://egghead.io/courses/professor-frisby-introduces-composable-functional-javascript
+  // 'do-your-own-map': { feedback: true },
+  // 'for-each': { feedback: true },
+  // 'do-your-own-for-each': { feedback: true },
+  // filter: { feedback: true },
+  // 'do-your-own-filter': { feedback: true },
+  // foldl: { feedback: true },
+  // foldr: { feedback: true },
+  // reduce: { feedback: true },
+  // 'do-your-own-reduce': { feedback: true },
+  // every: { feedback: true },
+  // some: { feedback: true },
+  'if-exists-then': { feedback: true },
+  // curry: { feedback: true },
+  // curry2: { feedback: true },
+  // 'do-your-own-curry': { feedback: true },
   // chunk
-  // curry
 }
