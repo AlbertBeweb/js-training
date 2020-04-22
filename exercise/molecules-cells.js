b6bf957ffc4c29dc867519add0b46d0423cac362
lee <lee-dasilva@hotmail.com>
2020-04-22 14:58:32 +0100

    corrections

--- exercise/molecules-cells.js
+++ exercise/molecules-cells.js
@@ -7,7 +7,7 @@ Write two functions :
   - `RNA` that given a DNA strand it must return is complement RNA
   - `DNA` that given a RNA strand it must return is complement DNA (invert RNA)
 
-Sense both strands are a sequence of nucleotides. It will be given the four nucleotides and its complements
+Since both strands are a sequence of nucleotides. It will be given the four nucleotides and its complements
 
 DNA | RNA
  G  -  C
